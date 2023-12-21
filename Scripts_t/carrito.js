b61a191ec18df91c961db31100bf8f1526db01ec
Rios06 <jenniriioss4@gmail.com>
2023-12-20 23:32:31 -0500

    JS carrito = funcion comprar carrito contenedor carro vacio

--- Scripts_t/carrito.js
+++ Scripts_t/carrito.js
@@ -130,4 +130,6 @@ botonComprar.addEventListener("click", comprarCarrito);
 function comprarCarrito() {
     productosEnCarrito.length = 0;
     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+    ontenedorCarritoVacio.classList.add("disabled");
 }
