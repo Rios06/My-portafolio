2176dafba342a6d9ea2b3c5c026dac6c295bfca5
Rios06 <jenniriioss4@gmail.com>
2023-12-20 23:02:24 -0500

    JS carrito = constante boten de vaciar

--- Scripts_t/carrito.js
+++ Scripts_t/carrito.js
@@ -6,3 +6,4 @@ const contenedorCarritoProductos = document.querySelector("#carrito-productos");
 const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
 const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
 let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
