433d3f3ca98938c2bc5881c26e1d851020283f17
Rios06 <jenniriioss4@gmail.com>
2023-12-20 21:56:58 -0500

    JS Add forEach para botones categoria completado

--- Scripts_t/main.js
+++ Scripts_t/main.js
@@ -38,5 +38,20 @@ fetch("./js/productos.json")
 }
 
 botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
 
-})
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
