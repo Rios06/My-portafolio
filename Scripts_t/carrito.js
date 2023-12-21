0e1f3c9f3df94221142d59eaae75e17d560ca54a
Rios06 <jenniriioss4@gmail.com>
2023-12-20 23:00:35 -0500

    JS carrito = constante contenedor de acciones en carrito

--- Scripts_t/carrito.js
+++ Scripts_t/carrito.js
@@ -3,3 +3,4 @@ productosEnCarrito = JSON.parse(productosEnCarrito);
 
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
 const contenedorCarritoProductos = document.querySelector("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
