12e8563b7d2400e9ae159e534cb7137c6d6171a2
Rios06 <jenniriioss4@gmail.com>
2023-12-20 22:39:15 -0500

    JS menu = Add close menu eventlisterner (f flecha)

--- Scripts_t/menu.js
+++ Scripts_t/menu.js
@@ -5,3 +5,7 @@ const aside = document.querySelector("aside");
 openMenu.addEventListener("click", () => {
     aside.classList.add("aside-visible");
 })
+
+closeMenu.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+})
