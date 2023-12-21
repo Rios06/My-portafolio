325ac5e2afe0aeae8bf9d3f1d7056acb83da40cf
Rios06 <jenniriioss4@gmail.com>
2023-12-20 23:26:13 -0500

    JS carrito = funcion eliminar del carrito completa

--- Scripts_t/carrito.js
+++ Scripts_t/carrito.js
@@ -69,3 +69,33 @@ function actualizarBotonesEliminar(){
     });
 }
     
+function eliminarDelCarrito(e) {
+    Toastify({
+        text: "Producto eliminado",
+        duration: 3000,
+        close: true,
+        gravity: "top", 
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', 
+            y: '1.5rem' 
+          },
+        onClick: function(){} 
+      }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    
+    productosEnCarrito.splice(index, 1);
+    cargarProductosCarrito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+}
