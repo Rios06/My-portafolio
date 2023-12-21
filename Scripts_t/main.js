7452592842fdc1ce6f44566c8c5a3a2153a461d8
Rios06 <jenniriioss4@gmail.com>
2023-12-20 22:47:58 -0500

    JS main = condicional para productosencarritoLs (para json y para que actualice el numero)

--- Scripts_t/main.js
+++ Scripts_t/main.js
@@ -67,3 +67,10 @@ function actualizarBotonesAgregar() {
 
 let productosEnCarrito;
 let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
