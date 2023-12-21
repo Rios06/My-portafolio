0b53aab6bc6b13a159519e3566e1db91fbc49f1c
Rios06 <jenniriioss4@gmail.com>
2023-12-20 23:03:40 -0500

    JS carrito = constante boton de comprar

--- Scripts_t/carrito.js
+++ Scripts_t/carrito.js
@@ -8,3 +8,4 @@ const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
 let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
 const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
 const contenedorTotal = document.querySelector("#total");
+const botonComprar = document.querySelector("#carrito-acciones-comprar");
