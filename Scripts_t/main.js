fa5323cd27552f2a8f4b9a3c56e937f37d3168d4
Rios06 <jenniriioss4@gmail.com>
2023-12-20 22:56:09 -0500

    JS main =funcion actualizar numerito terminada

--- Scripts_t/main.js
+++ Scripts_t/main.js
@@ -112,5 +112,6 @@ function agregarAlCarrito(e) {
 }
 
 function actualizarNumerito(){
-    
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
 }
