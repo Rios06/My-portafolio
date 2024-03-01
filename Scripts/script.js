d79084697ff41ce496ca4ada03482dce28a1c1a6
Rios06 <jenniriioss4@gmail.com>
2024-03-01 17:41:26 -0500

    Fetch proyect

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,4 @@
-//FUNCION PARA QUE CUANDO SELECCIONES UNA PARTE DEL MENU SE QUITEN LAS OTRAS
+
 function seleccionar(link){
     let opciones = document.querySelectorAll('#links a');
     opciones[0].className = "";
@@ -9,15 +9,11 @@ function seleccionar(link){
     link.className = "seleccionado";
 
 
-    //Se desaparece el menu despues de que seleccionen una opcion pero en el modo responsive
 
     let x= document.getElementById("nav");
     x.className = "";
 
 }
-
-
-//FUNCION MENU responsive
 
 function responsiveMenu(){
     let x = document.getElementById("nav");
@@ -27,4 +23,44 @@ function responsiveMenu(){
         x.className = "";
     }
 }
+
+let numbers = document.querySelectorAll(".porcentaje");
+let circles = document.querySelectorAll("circle");
+
+numbers.forEach((number, index) => {
+    let contador = 0;
+    setInterval(() => {
+        if(contador == parseInt(number.textContent)) {
+            clearInterval();
+        } else {
+            contador += 1;
+            number.textContent = contador + "%";  
+            let porcentaje = (472 * (100 - contador)) / 100;
+            circles[index].style.strokeDashoffset = porcentaje;
+        }
+    }, 30);
+});
+
+
+
+function escribirNombre(nombre, elemento) {
+    let i = 0;
+    setInterval(() => {
+        if (i < nombre.length) {
+            elemento.textContent += nombre[i];
+            i++;
+        } else {
+            i = 0;
+            elemento.textContent = ''; 
+        }
+    }, 150); 
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const nombre = "Jennifer Rios";
+    const nombreElemento = document.getElementById('nombre');
+    escribirNombre(nombre, nombreElemento);
+});
+
 
